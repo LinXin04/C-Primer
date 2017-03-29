774a681109e3eae823c118e2791741c0185fb467
LinXin04 <1647399840@qq.com>
2017-03-29 20:40:06 +0800

    程序清单3.11
    不同类型数据做除法

---
+++ divide.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Integer division: 9/5 = " << 9 / 5 << endl;
+	cout << "Floating-point division: 9.0/5.0 = " << 9.0 / 5.0 << endl;
+	cout << "Mixed division: 9.0/5 = " << 9.0 / 5 << endl;
+	cout << "Double constants: 1e7/9.0 = " << 1e7 / 9.0 << endl;
+	cout << "Float constants: 1e7f/9.0f = " << 1e7f / 9.0f << endl;
+	return 0;
+
+}
