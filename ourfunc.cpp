ec6cf94c4f21a74f6e7bb02e13eb4d4b54e45c4a
LinXin04 <1647399840@qq.com>
2017-03-29 20:12:45 +0800

    程序清单2.5
    用户自定义函数

---
+++ ourfunc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void simon(int);
+
+int main()
+{
+	simon(3);
+	cout << "Pick an integer: ";
+	int count;
+	cin>>count;
+	simon(count);
+	cout << "Done!" << endl;
+	return 0;
+}
+
+void simon(int n)
+{
+	cout << "Simon says touch your toes " << n << " times." << endl;
+}
