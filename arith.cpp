8dadb3fcab101d1f26812f6f22c0a86cafb4eea5
LinXin04 <1647399840@qq.com>
2017-03-29 20:38:51 +0800

    程序清单3.10
    C++运算符的使用

---
+++ arith.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float hats, heads;
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+	cout << "Enter a number: ";
+	cin >> hats;
+	cout << "Enter another number: ";
+	cin >> heads;
+	cout << "hats = " << hats << "; heads = " << heads << endl;
+	cout << "hats + heads = " << hats + heads << endl;
+	cout << "hats - heads = " << hats - heads << endl;
+	cout << "hats * heads = " << hats*heads << endl;
+	cout << "hats / heads = " << hats / heads << endl;
+	return 0;
+
+
+}
