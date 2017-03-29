dbd953988eff60553fca14b04b52a73b2d3e56f3
LinXin04 <1647399840@qq.com>
2017-03-29 20:36:06 +0800

    程序清单3.7
    转义字符的表示

---
+++ bondini.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "\aOperation \"HyperHype\" is now activated!\n";
+	cout << "Enter your agent code:_______\b\b\b\b\b\b\b";
+	long code;
+	cin >> code;
+	cout << "\aYou entered " << code << " ...\n";
+	cout << "\aCode verified! Proceed with Plan Z3!\n";
+	return 0;
+}
+
