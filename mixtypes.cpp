c44041c793023e45cb3bc494f1bac568f5fcab1a
LinXin04 <1647399840@qq.com>
2017-03-29 21:03:44 +0800

    程序清单4.23
    不同类型的混用

---
+++ mixtypes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct antarctica_years_end
+{
+	int year;
+};
+
+int main()
+{
+	antarctica_years_end s01, s02, s03;
+	s01.year = 1998;
+	antarctica_years_end *pa = &s02;
+	pa->year = 1999;
+	antarctica_years_end trio[3];
+	trio[0].year = 2003;
+
+	cout << trio->year << endl;
+	
+	const antarctica_years_end *arp[3] = { &s01, &s02, &s03 };
+	cout << arp[1]->year << endl;
+
+	const antarctica_years_end **ppa = arp;
+	auto ppb = arp;
+
+	cout << (*ppa)->year << endl;
+	cout << (*(ppa + 1))->year << endl;
+	return 0;
+}
