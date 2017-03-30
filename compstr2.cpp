d56cf83e04e62a36774e5af62b08a9427fa838dc
LinXin04 <1647399840@qq.com>
2017-03-30 21:59:12 +0800

    程序清单5.12
    string类的字符串可以直接比较

---
+++ compstr2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string word = "?ate";
+	for (char ch = 'a'; word != "mate"; ch++)
+	{
+		cout << word << endl;
+		word[0] = ch;
+	}
+	cout << "After loop ends, word is " << word << endl;
+	return 0;
+}
