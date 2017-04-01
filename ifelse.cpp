afe343b4d05061c29fd18e49ff8d19468267b3a6
LinXin04 <1647399840@qq.com>
2017-04-01 14:44:26 +0800

    程序清单6.2
    ifelse的使用

---
+++ ifelse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char ch;
+	cout << "Type, and I shall repeat." << endl;
+	cin.get(ch);
+	while (ch != '.')
+	{
+		if (ch == '\n')
+			cout << ch;
+		else
+			cout << ++ch;
+		cin.get(ch);
+	}
+	cout << "\nPlease excuse the slight confusion.\n";
+	return 0;
+}
