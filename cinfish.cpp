18b624185ee3ba1778e584fb1170a72e51a859a0
LinXin04 <1647399840@qq.com>
2017-04-03 10:52:31 +0800

    程序清单6.13
    读入数字

---
+++ cinfish.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+const int Max = 5;
+int main()
+{
+	double fish[Max];
+	cout << "Please enter the weights of your fish.\n";
+	cout << "You may enter up to " << Max << " fish <q to terminate>.\n";
+	cout << "fish #1: ";
+	int i=0;
+	while (i < Max && cin >> fish[i])
+	{
+		if (++i < Max)
+			cout << "fish #" << i + 1 << ": ";
+	}
+
+	double total = 0.0;
+	for (int j = 0; j < i; j++)
+		total += fish[j];
+	if (i == 0)
+		cout << "No fish.\n";
+	else
+		cout << total / i << " = average weight of " << i << " fish\n";
+	cout << "Done!" << endl;
+	return 0;
+}
