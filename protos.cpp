27f5d03ed1e6f18f0667ba1fbe624c5f879f351b
LinXin04 <1647399840@qq.com>
2017-04-04 17:27:12 +0800

    程序清单7.2
    有返回值与无返回值的函数的调用

---
+++ protos.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+void cheers(int);
+double cube(double);
+int main()
+{
+	cheers(5);
+	cout << "Give me a number: ";
+	double side;
+	cin >> side;
+	double volume = cube(side);
+	cout << "A" << side << "-foot cube has a volume of ";
+	cout << volume << " cubic feet.\n";
+	cheers(cube(2));
+	return 0;
+}
+
+void cheers(int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << "Cheers!";
+	cout << endl;
+}
+double cube(double x)
+{
+	return x*x*x;
+}
