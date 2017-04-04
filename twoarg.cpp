a76a8a910ddc0542548311945757a88ec9088501
LinXin04 <1647399840@qq.com>
2017-04-04 17:56:03 +0800

    程序清单7.3
    多个参数的函数调用

---
+++ twoarg.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void n_chars(char, int);
+int main()
+{
+	int times;
+	char ch;
+	cout << "Enter a character: ";
+	cin >> ch;
+	while (ch != 'q')
+	{
+		cout << "Enter an integer: ";
+		cin >> times;
+		n_chars(ch, times);
+		cout << "\nEnter another character or press the q-key to quit: " ;
+		cin >> ch;
+	}
+	cout << "The value of times is " << times << ".\n";
+	return 0;
+}
+void n_chars(char c, int n)
+{
+	while (n-- > 0)
+		cout << c;
+}
