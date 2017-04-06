98fc7e49a674430e1a3ff6eb6d1c64bf1638806b
LinXin04 <1647399840@qq.com>
2017-04-06 19:55:28 +0800

    程序清单7.12
    结构与函数2

---
+++ atrctfun.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct polar
+{
+	double distance;
+	double angle;
+};
+struct rect
+{
+	double x;
+	double y;
+};
+polar rect_to_polar(rect xypos);
+void show_polar(polar dapos);
+int main()
+{
+	polar pplace;
+	rect rplace;
+	cout << "Enter the x and y value: ";
+	while (cin >> rplace.x >> rplace.y)
+	{
+		pplace = rect_to_polar(rplace);
+		show_polar(pplace);
+		cout << "Next two numbers (q to quit): ";
+	}
+	cout << "Done!" << endl;
+	return 0;
+}
+polar rect_to_polar(rect xypos)
+{
+	polar answer;
+	answer.distance = sqrt(xypos.x*xypos.x + xypos.y*xypos.y);
+	answer.angle = atan2(xypos.y, xypos.x);
+	return answer;
+}
+void show_polar(polar dapos)
+{
+	const double Rad_to_deg = 57.29577951;
+	cout << "distance = " << dapos.distance;
+	cout << ", angle = " << dapos.angle*Rad_to_deg;
+	cout << " degrees" << endl;
+}
