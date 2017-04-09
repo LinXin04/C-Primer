fc8624bd3fa08001ccecfd41fec195e0f15c634b
LinXin04 <1647399840@qq.com>
2017-04-09 13:38:47 +0800

    程序清单8.2
    创建引用变量

---
+++ firstref.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int rats = 101;
+	int & rodents = rats;
+	cout << "rats = " << rats;
+	cout << ", rodents = " << rodents << endl;
+	rodents++;
+	cout << "rats = " << rats;
+	cout << ", rodents = " << rodents << endl;
+
+	cout << "rats address = " << &rats;
+	cout << ", rodents address = " << &rodents << endl;
+	return 0;
+}
