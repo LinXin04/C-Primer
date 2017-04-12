17ade4fa4b5eefdc52767530b400347532d9f6cd
LinXin04 <1647399840@qq.com>
2017-04-12 21:37:38 +0800

    程序清单8.14
    部分排序规则

---
+++ temptempover.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+void ShowArray(T arr[], int n);
+
+template <typename T>
+void ShowArray(T *arr[], int n);
+
+struct debts
+{
+	char name[50];
+	double amount;
+};
+int main()
+{
+	int things[6] = { 13, 31, 103, 301, 310, 130 };
+	struct debts mr_E[3] = {
+		{ "Ima Wolfe", 2400.0},
+		{ "Ura Foxe",1300.0},
+		{ "Iby Stout",1800.0 }
+	};
+	double *pd[3];
+
+	for (int i = 0; i < 3; i++)
+		pd[i] = &mr_E[i].amount;
+
+	cout << "Listing Mr.E's counts of things:" << endl;
+	ShowArray(things, 6);
+
+	cout << "Listing Mr.E's debts:" << endl;
+	ShowArray(pd, 3);
+	return 0;
+}
+
+template <typename T>
+void ShowArray(T arr[], int n)
+{
+	cout << "template A" << endl;
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << ' ';
+	cout << endl;
+}
+
+template <typename T>
+void ShowArray(T * arr[], int n)
+{
+	cout << "template B" << endl;
+	for (int i = 0; i < n; i++)
+		cout << *arr[i] <<' '<< endl;
+	cout << endl;
+}
