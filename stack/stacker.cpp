846b676080192a915ba398f85bb8303c4a215345
LinXin04 <1647399840@qq.com>
2017-05-02 21:49:21 +0800

    程序清单10.10-10.12
    抽象数据类型ADT

---
+++ stack/stacker.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cctype>
+#include "stack.h"
+using namespace std;
+int main()
+{
+	Stack st;
+	char ch;
+	unsigned long po;
+	cout << "Please enter A to add a purchase order,\n"
+		<< "P to process a PO, or Q to quit." << endl;
+	while (cin >> ch&&toupper(ch) != 'Q')
+	{
+		while (cin.get() != '\n')
+			continue;
+		if (!isalpha(ch))
+		{
+			cout << '\a';
+			continue;
+		}
+		switch (ch)
+		{
+		case 'A':
+		case 'B':cout << "Enter a PO number to add: ";
+			cin >> po;
+			if (st.isfull())
+				cout << "Stack already full\n";
+			else
+				st.push(po);
+			break;
+		case 'P':
+		case 'p':if (st.isempty())
+					cout << "stack already empty\n";
+				 else
+				 {
+					 st.pop(po);
+					 cout << "PO #" << po << " popped\n";
+				 }
+				 break;
+		}
+		cout << "Please enter A to add a purchase order,\n"
+			<< "P to process a PO, or Q to quit.\n";
+	}
+	cout << "Bye\n";
+	return 0;
+}
