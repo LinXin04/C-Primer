5c9a2aca3b5b615fc9f17e9798cd3a48218a176e
LinXin04 <1647399840@qq.com>
2017-05-03 16:50:46 +0800

    程序清单11.1-11.3
    计算时间

---
+++ mytime0/usetime0.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "mytime0.h"
+using namespace std;
+int main()
+{
+	Time planning;
+	Time coding(2, 40);
+	Time fixing(5, 55);
+	Time total;
+
+	cout << "planning time = ";
+	planning.Show();
+	cout << endl;
+
+	cout << "coding time = ";
+	coding.Show();
+	cout << endl;
+
+	cout << "fixing time = ";
+	fixing.Show();
+	cout << endl;
+
+	total = coding.Sum(fixing);
+
+	cout << "coding.Sum(fixing) = ";
+	total.Show();
+	cout << endl;
+
+	return 0;
+
+}
