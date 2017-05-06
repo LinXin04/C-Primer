52d6e07c90d9cb82bce1f59708ab597a9d7633fa
LinXin04 <1647399840@qq.com>
2017-05-06 12:26:47 +0800

    程序清单11.19-11.21
    转换函数

---
+++ stonewt1/stonewt1.h
@@ -0,0 +1,20 @@
+#ifndef STNOEWT1_H_
+#define STONEWT1_H_
+class Stonewt
+{
+private:
+	enum {Lbs_per_stn=14};
+	int stone;
+	double pds_left;
+	double pounds;
+public:
+	Stonewt(double lbs);
+	Stonewt(int stn, double lbs);
+	Stonewt();
+	~Stonewt();
+	void show_lbs()const;
+	void show_stn()const;
+	operator int() const;
+	operator double() const;
+};
+#endif
